e6e837bdf83f6e90a8c9640b11e08ae4417a7fcf
HarshanaWalpita <harshanawalpita@gmail.com>
2021-07-10 23:26:24 +0530

    modified buyer ui

--- client/src/components/buyer/posts/Posts.js
+++ client/src/components/buyer/posts/Posts.js
@@ -13,59 +13,61 @@ function Posts() {
     return(
         <div className="posts-b">
             <div className="posts__container-b">
-                <div className="search-bar-box-b">
-                    <div className="post-search-box-b">
-                        <input type="text" placeholder="What are you looking for?"></input>
-                        <i className="fas fa-search"></i>
+                <div className="search-box-shadow-b">
+                    <div className="search-bar-box-b">
+                        <div className="post-search-box-b">
+                            <input type="text" placeholder="What are you looking for?"></input>
+                            <i className="fas fa-search"></i>
+                        </div>
+                        <div className="search-button-b">
+                            <input type="submit" value="Search"></input>
+                        </div>
                     </div>
-                    <div className="search-button-b">
-                        <input type="submit" value="Search"></input>
-                    </div>
-                </div>
-                <div className="search-bar-b">
-                    <div className="box-b">
-                        <h3>Waste Type</h3>
-                        <select>
-                            <option>All</option>
-                            <option>Polythene</option>
-                            <option>Plastic</option>
-                            <option>Organic Waste</option>
-                            <option>Paper</option>
-                            <option>Metal</option>
-                        </select>
-                    </div>
-                    <div className="box-b">
-                        <h3>Waste Item</h3>
-                        <select>
-                            <option>All</option>
-                            <option>Bag</option>
-                            <option>Bucket</option>
-                            <option>Plate</option>
-                            <option>Paper</option>
-                            <option>Chair</option>
-                        </select>
-                    </div>
-                    <div className="box-b">
-                        <h3>Quantity</h3>
-                        <select>
-                            <option>All</option>
-                            <option>1 kg</option>
-                            <option>2 kg</option>
-                            <option>3 kg</option>
-                            <option>4 kg</option>
-                            <option>5 kg</option>
-                        </select>
-                    </div>
-                    <div className="box-b">
-                        <h3>Location</h3>
-                        <select>
-                            <option>All</option>
-                            <option>Gampaha</option>
-                            <option>Miriswatte</option>
-                            <option>Kadawatha</option>
-                            <option>Imbulgoda</option>
-                            <option>Yakkala</option>
-                        </select>
+                    <div className="search-bar-b">
+                        <div className="box-b">
+                            <h3>Waste Type</h3>
+                            <select>
+                                <option>All</option>
+                                <option>Polythene</option>
+                                <option>Plastic</option>
+                                <option>Organic Waste</option>
+                                <option>Paper</option>
+                                <option>Metal</option>
+                            </select>
+                        </div>
+                        <div className="box-b">
+                            <h3>Waste Item</h3>
+                            <select>
+                                <option>All</option>
+                                <option>Bag</option>
+                                <option>Bucket</option>
+                                <option>Plate</option>
+                                <option>Paper</option>
+                                <option>Chair</option>
+                            </select>
+                        </div>
+                        <div className="box-b">
+                            <h3>Quantity</h3>
+                            <select>
+                                <option>All</option>
+                                <option>1 kg</option>
+                                <option>2 kg</option>
+                                <option>3 kg</option>
+                                <option>4 kg</option>
+                                <option>5 kg</option>
+                            </select>
+                        </div>
+                        <div className="box-b">
+                            <h3>Location</h3>
+                            <select>
+                                <option>All</option>
+                                <option>Gampaha</option>
+                                <option>Miriswatte</option>
+                                <option>Kadawatha</option>
+                                <option>Imbulgoda</option>
+                                <option>Yakkala</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
                 <main className="grid-b">
