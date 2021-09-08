534686f65228719e4c0448ede8037e5ed4fd92df
ChathuPriya-18 <hacpriya@gmail.com>
2021-09-08 18:22:49 +0530

    moinor changes

--- client/src/components/company/components/company/posts/AddPost.js
+++ client/src/components/company/components/company/posts/AddPost.js
@@ -217,7 +217,7 @@ function AddPost() {
                                         )}
                                     </div>
                                     <div className="input-box-c">
-                                        <span className="date-c">Available Date</span>
+                                        <span className="date-c">Required Date</span>
                                         <input type="date" name="avbDate" id="avbDate" placeholder="Enter date" value={formValues.avbDate}
                                                onChange={handleChange}
                                                className={formErrors.avbDate && "input-error"}></input>
